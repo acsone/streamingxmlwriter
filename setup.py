43de1c5b5c4638f75d95aaae977d01c6f1160774
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2016-02-16 18:07:09 +0100

    packaging

--- setup.py
+++ setup.py
@@ -6,4 +6,17 @@ setup(
                 "streaming xml writer",
     version="1.0.0",
     packages=["streamingxmlwriter"],
+    install_requires=["six"],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: '
+        'GNU Lesser General Public License v3 (LGPLv3)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+    license='LGPLv3',
+    author='ACSONE SA/NV',
+    author_email='info@acsone.eu',
+    url='http://github.com/acsone/streamingxmlwriter',
 )
