528c12537f10cdfb8f68747a084eaa3101ed64f6
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2016-02-17 16:58:47 +0100

    short copyright header

--- streamingxmlwriter/core.py
+++ streamingxmlwriter/core.py
@@ -1,25 +1,6 @@
 # -*- coding: utf-8 -*-
-#########################################################################
-#
-# StreamingXMLWriter
-#
-# Copyright (c) 2015-2016 ACSONE SA/NV, All rights reserved.
-# Author: Stéphane Bidoul <stephane.bidoul@acsone.eu>
-#
-# This python module is free software; you can redistribute it and/or
-# modify it under the terms of the GNU Lesser General Public
-# License as published by the Free Software Foundation; either
-# version 3.0 of the License, or (at your option) any later version.
-#
-# This python library is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# Lesser General Public License for more details.
-#
-# You should have received a copy of the GNU Lesser General Public
-# License along with this python module.
-#
-#########################################################################
+# © 2016 ACSONE SA/NV
+# License LGPLv3 (http://www.gnu.org/licenses/lgpl-3.0-standalone.html)
 """ streamingxmlwriter
 
 A lightweight pythonic standard compliant streaming xml writer.
